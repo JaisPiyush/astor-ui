15f330ca6ba19d7594ba3f83d3066e916fa85aa2
TAN95t <tanaymaheshwari54@gmail.com>
2023-11-23 00:57:02 +0530

    tables added to dashboard and landing page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,8 @@
 import { Paper, Stack, Divider, Typography } from "@mui/material";
 import { NavLink, Outlet } from 'react-router-dom';
 import { Wrapper } from "../assets/wrapper/NavLinkWrapper";
+import DashboardTableCompoenent from "../components/DashboardTableComponent";
+
 
 function Dashboard() {
     return (
@@ -9,13 +11,18 @@ function Dashboard() {
                 <Paper elevation={3}>
                     <Stack direction="row">
                         <Stack direction="column" sx={{ width: "60%", alignItems: "center" }}>
-                            <h1>part1</h1>
+                            <Stack sx={{ height: 50, width: "100%", backgroundColor: "#607d8b36", alignItems: "center"}}>
+                                <Typography sx={{ height: "100%", fontSize: 30 }}>
+                                    Heading Comes Here 
+                                </Typography>
+                            </Stack>
+                            <DashboardTableCompoenent />
                         </Stack>
                         <Divider orientation="vertical" flexItem />
                         <Stack direction="column" sx={{ width: "40%", alignItems: "center" }}>
                             <Wrapper><Stack direction="row" sx={{ width: "100%", justifyContent: "space-around", padding: 0, backgroundColor: "#607d8b36", height: 50 }}>
-                                <NavLink className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')} to="/dashboard/index"><Typography sx={{height: "100%", fontSize: 30}}>Index</Typography></NavLink>
-                                <NavLink className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')} to="/dashboard/cashpool"><Typography sx={{height: "100%", fontSize: 30}}>Cash Pool</Typography></NavLink>
+                                <NavLink className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')} to="/dashboard/index"><Typography sx={{ height: "100%", fontSize: 30 }}>Index</Typography></NavLink>
+                                <NavLink className={({ isActive }) => (isActive ? 'nav-link-active' : 'nav-link')} to="/dashboard/cashpool"><Typography sx={{ height: "100%", fontSize: 30 }}>Cash Pool</Typography></NavLink>
                             </Stack></Wrapper>
                             <Stack direction="row" sx={{ margin: 2, width: "95%", padding: 1, justifyContent: "center" }}>
                                 <Paper elevation={1} sx={{ width: "100%" }}>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,15 @@
+import {Stack } from "@mui/material"
+import LandingTableCompoenent from "../components/LandingTableComponent"
+
+
 
 function Landing() {
   return (
-    <div>Landing Page</div>
+    <Stack sx={{ width: "100%", height: "100%", padding: 3, backgroundColor: "#607d8b36" }}>
+      <Stack sx={{ padding: 2, width: "100%", height: "100%" }}>
+        <LandingTableCompoenent />
+      </Stack>
+    </Stack>
   )
 }
 
