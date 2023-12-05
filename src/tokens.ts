425a99322e40499019a323b12487a9d6d8a0872d
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-12-05 23:08:52 +0530

    address change

--- src/tokens.ts
+++ src/tokens.ts
@@ -4,7 +4,7 @@ export const tokens: Token[] = [
     {
         name: 'Mixed Four',
         symbol: 'MIX4',
-        address: '0xD38ac076C909C8fF8D9f617422dfd51ec256AbaC',
+        address: '0x2132A0A7f298bB2a55D035dfe568E191d6b4b1Ae',
         url: 'https://i.pinimg.com/736x/ae/63/70/ae6370269e0421920a5b4ab34ebe7387.jpg'
     }
 ]
@@ -12,46 +12,46 @@ export const tokens: Token[] = [
 export const indexTokens = [tokens[0]];
 
 export const tokensData: Record<string, Token> = {
-    '0xaec8eE023A1b944f65aaAd7Ce4dB400065041d12': {
-        address: '0xaec8eE023A1b944f65aaAd7Ce4dB400065041d12',
+    '0x1ed1029d010934d0E114e2153ab1053919169b29': {
+        address: '0x1ed1029d010934d0E114e2153ab1053919169b29',
         name: 'Bitcoin',
         symbol: 'BTC',
         url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png'
     },
-    '0xd2FaAf51d5657a11619f2f30585435337c2DA1Bb': {
-        address: '0xd2FaAf51d5657a11619f2f30585435337c2DA1Bb',
+    '0x8735D105C73291F59E4791Acd0A5978BCCace42D': {
+        address: '0x8735D105C73291F59E4791Acd0A5978BCCace42D',
         name: 'Chainlink',
         symbol: 'LINK',
         url: 'https://cryptologos.cc/logos/chainlink-link-logo.png'
     },
-    '0xbA255629B402cbB9B8bD53ACF905e6255aebc296': {
-        address: '0xbA255629B402cbB9B8bD53ACF905e6255aebc296',
+    '0xAe8beF3454E440fC6a8EA4960400693449973298': {
+        address: '0xAe8beF3454E440fC6a8EA4960400693449973298',
         name: 'Binance',
         symbol: 'BNB',
         url: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Binance-coin-bnb-logo.png'
     },
-    '0xdF5CC6883fefe255E50d1A0ceA6DB5c0Df87CaFB': {
-        address: '0xdF5CC6883fefe255E50d1A0ceA6DB5c0Df87CaFB',
+    '0x14281ff9a6EcC889cFb0c94c327262569f9F661F': {
+        address: '0x14281ff9a6EcC889cFb0c94c327262569f9F661F',
         name: 'Polygon',
         symbol: 'MATIC',
         url: 'https://cryptologos.cc/logos/polygon-matic-logo.png'
     },
-    '0x557838aB4b0e37B20741827fe27B38DDfa68CE9A': {
-        address: '0x557838aB4b0e37B20741827fe27B38DDfa68CE9A',
+    '0x2e961FdC50C4e0fdFb06ebce49Be6E857E03FbD9': {
+        address: '0x2e961FdC50C4e0fdFb06ebce49Be6E857E03FbD9',
         name: 'USDT',
         symbol: 'USDT',
         url: 'https://w7.pngwing.com/pngs/840/253/png-transparent-usdt-cryptocurrencies-icon-thumbnail.png'
     },
-    '0xD38ac076C909C8fF8D9f617422dfd51ec256AbaC': {
+    '0x2132A0A7f298bB2a55D035dfe568E191d6b4b1Ae': {
         name: 'Mixed Four',
         symbol: 'MIX4',
-        address: '0xD38ac076C909C8fF8D9f617422dfd51ec256AbaC',
+        address: '0x2132A0A7f298bB2a55D035dfe568E191d6b4b1Ae',
         url: 'https://i.pinimg.com/736x/ae/63/70/ae6370269e0421920a5b4ab34ebe7387.jpg'
     }
 }
 
 export const cashPools: Record<string, string> = {
-    '0xD38ac076C909C8fF8D9f617422dfd51ec256AbaC': '0x0B05d210d8eC00f640Fd147cC4831D438A054fF7'
+    '0x2132A0A7f298bB2a55D035dfe568E191d6b4b1Ae': '0xC4F10f35326f648954b9264cD1771b0dFf977016'
 }
 
-export const USDT = '0x557838aB4b0e37B20741827fe27B38DDfa68CE9A'
+export const USDT = '0x2e961FdC50C4e0fdFb06ebce49Be6E857E03FbD9'
